3b1d971a6a9ab80978d51ef63c9326cb42fefec4
NahuelArias <nahuel.arias@ipernet.net.ar>
2024-01-22 22:52:11 -0300

    Primer commit

---
+++ src/app/layouts/dashboard/pages/users/users.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
+import {MatTableModule} from '@angular/material/table';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+@NgModule({
+  declarations: [
+    UsersComponent,
+    UserFormComponent
+  ],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+  exports: [
+    UsersComponent
+  ],
+})
+export class UsersModule { }
